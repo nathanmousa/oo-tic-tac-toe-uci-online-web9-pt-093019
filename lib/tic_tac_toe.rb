e1309b116c53726bea2c8bd3418f3956554ef124
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-31 01:38:17 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,46 @@
+class TicTacToe
+  attr_accessor :board
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(raw_input)
+    input = (raw_input.to_i) - 1
+    return input
+  end
+  
+  def move(input, token = "X")
+    @board[input] = token
+  end
+  
+  def position_taken?(input)
+    if @board[input] == " "
+      return false
+    else
+      return true
+    end
+  end
+  
+  def valid_move?(input)
+    input.between?(0, 8) && !position_taken?(input)
+  end
+  
+  def turn
+    
+  end
+  
+  def turn_count
+    
+  end
+  
+end
